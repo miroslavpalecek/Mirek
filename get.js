25ec267dc2540ff145a256b7322f89cd52cc3162
miroslavpalecek <miroslavpalecek@gmail.com>
2020-11-04 19:41:29 +0100

    array

--- get.js
+++ get.js
@@ -1,7 +1,19 @@
-function getNigger(req, res) {
-res.json({ message: "Ahoj negri"});
-}
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
-module.exports = getNigger;
+function get(req, res) {
+  
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("CRM");
+    dbo.collection("contacts").find().toArray( function(err, result) {
+      res.json(result);
+      if (err) throw err;
+      db.close();
+    });
+  });
 
-
+  //res.json({ message: "Ahoj negri"});
+  }
+  
+  module.exports = get;
